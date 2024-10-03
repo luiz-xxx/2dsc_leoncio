4559b69aecc2a46d57dd5b0f39b09514b0ea861d
luiz-xxx <luizvictorsavulski@gmail.com>
2024-10-03 15:21:56 +0000

    iniciado o codigo  javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,14 @@
+//FAZ REFERENCIA AOS ELEMENTOS DOM
+const numero1 = document.querySelector("#numero1")
+const numero2 = document.querySelector("#numero2")
+const btnCalcular = document.querySelector("#btn-calcular")
+const resultado = document.querySelector(".resultado")
+const divisao = document.querySelector(".divisao")
+const adiao = document.querySelector(".adicao")
+const subtracao = document.querySelector(".subtracao")
+const multiplicacao = document.querySelector(".multiplicacao")
+
+//CRIA A FUNÇÃO CALCULAR
+
+
+//ADICIONA ESCUTAR DE EVENTOS
